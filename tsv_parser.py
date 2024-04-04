622c3718d9befbaa9fa7d75bc75163e9d5b34b58
Shaskels <allasidner028@gmail.com>
2024-04-04 17:55:34 +0700

    mMZ,MCZ,

--- tsv_parser.py
+++ tsv_parser.py
@@ -6,14 +6,19 @@ from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+sample = []
 
 def process_the_data(file_name) :
     nltk.download('stopwords')
     dataset = pd.read_csv(file_name, delimiter = ':')
-    corpus = [] 
-    for i in range(len(dataset['Comment'])):
+    X, y = shuffle(dataset['Comment'], dataset.iloc[:,1].values)
+    corpus = []
+    for i in range(len(X)):
+        sample.append(X[i])
         #убираем все символы которые не явл буквами 
-        comment = re.sub('[^а-яА-Я]', ' ', dataset['Comment'][0])
+        comment = re.sub('[^а-яА-Я]', ' ', X[i])
         #переводим в нижний регистр и разделяем на слова
         comment = comment.lower().split()
         ps = SnowballStemmer("russian")
@@ -27,6 +32,4 @@ def process_the_data(file_name) :
 
     cv = CountVectorizer(max_features = 1500) 
     X = cv.fit_transform(corpus).toarray() 
-    y = dataset.iloc[:,1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
-    return X_train, X_test, y_train, y_test
+    return X, y

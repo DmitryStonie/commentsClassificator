3d50e6b472f2d687a06b9a0386b1fb99fb725542
Shaskels <allasidner028@gmail.com>
2024-04-05 15:38:38 +0700

    Confusion matrix

--- classificator.py
+++ classificator.py
@@ -3,6 +3,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 from prettytable import PrettyTable
 import tsv_parser as par
 
@@ -18,13 +22,12 @@ def print_results(num,y_test,predicted):
 
     td = td[:]
     table = PrettyTable(th)
-    table._max_width = {"Комментарий":50}
+    table._max_width = {"Комментарий":70}
     while td:
         table.add_row(td[:3])
         td = td[3:]
     
     print(table)
- 
 
 def naive_bayes(X_train, y_train, X_test):
     model = MultinomialNB()
@@ -32,7 +35,7 @@ def naive_bayes(X_train, y_train, X_test):
     return model.predict(X_test)
 
 def neighbors(X_train, y_train, X_test):
-    model = KNeighborsClassifier(3)
+    model = KNeighborsClassifier(5)
     model.fit(X_train, y_train)
     return model.predict(X_test)
 
@@ -47,9 +50,24 @@ def SVC(X_train, y_train, X_test):
     return model.predict(X_test)
 
 def train(model_func):
+    #подготавливаем обучающую выборку
     X_train, y_train = par.process_the_data("train_sample.tsv")
-    X_test, y_test = par.process_the_data("train_sample.tsv")
+    #подготавливаем тестовую выборку
+    X_test, y_test = par.process_the_data("test_sample.tsv")
     predicted = model_func(X_train,y_train,X_test)
     score_test = accuracy_score(y_test, predicted)
+    #Выводим первые 10 комментариев и их оценки
     print_results(10,y_test,predicted)
+    #Выводим точность на тестовой выборке
     print(f"\n \033[32;40m Точность на тестовой выборке: {score_test}")
+    #Вывод матрицы ошибок
+    cm = confusion_matrix(y_test, predicted)
+    sns.heatmap(cm,
+                annot=True,
+                fmt='g',
+                xticklabels=['Плохо','Хорошо'],
+                yticklabels=['Плохо','Хорошо'])
+    plt.ylabel('Prediction',fontsize=13)
+    plt.xlabel('Actual',fontsize=13)
+    plt.title('Confusion Matrix',fontsize=17)
+    plt.show()
